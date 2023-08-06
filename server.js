f8216c545a597697dee57e8becded79d97a2748a
gangwar <ashishgangwar231@gmail.com>
2023-08-06 20:45:13 +0530

    last commit

--- server.js
+++ server.js
@@ -10,16 +10,19 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.status(200).send('Hello world!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const {first,second} = req.body;
+    console.log(first)
+    res.status(200).json({ "result": first+second });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const {first,second} = req.body;
+    res.status(200).json({ "result": first-second });
 });
 
 app.use(baseUrl, baseRouter);
